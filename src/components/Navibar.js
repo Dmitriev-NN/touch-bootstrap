fa6b7f44ad5478e969da67baa408f52f86c3acf1
Dmitriev Nikolay <dmitriev.nn.138@gmail.com>
2022-06-09 19:42:32 +0300

    Auth context added

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,14 +1,22 @@
-import { Navbar, Nav, Button, Container, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 //Context
 import { AuthContext } from '../context/index';
 import { useContext } from 'react'
 
 export const Navibar = () => {
-    // const [isAuth, setAuth] = useContext(AuthContext);
+    const { isAuth, setAuth } = useContext(AuthContext);
+
+    const login = () => {
+        sessionStorage.setItem('auth', 'true');
+        setAuth(true);
+        console.log(sessionStorage);
+        console.log('login')
+    }
 
     const exit = () => {
-        // sessionStorage.setItem('auth', 'false');
-        // setAuth(false);
+        sessionStorage.setItem('auth', 'false');
+        setAuth(false);
+        console.log(sessionStorage);
         console.log('exit')
     }
 
@@ -18,15 +26,44 @@ export const Navibar = () => {
                 <Navbar.Brand className='ms-5'>WebDevBlog</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse>
-                    <Nav className="me-auto">
-                        <Nav.Link href='/home'>Home</Nav.Link>
-                        <Nav.Link href='/users'>Users</Nav.Link>
-                        <Nav.Link href='/about'>About</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href='#login'>Login</Nav.Link>
-                        <Nav.Link href='#exit' onClick={exit}>Exit</Nav.Link>
-                    </Nav>
+                    {isAuth ?
+                    <>
+                        <Nav>
+                            <Nav.Link href='/home'>Home</Nav.Link>
+                            <Nav.Link href='/users'>Users</Nav.Link>
+                            <Nav.Link href='/about'>About</Nav.Link>
+                        </Nav>
+                        <Nav className='ms-auto'>
+                            <Nav.Link href='#exit' onClick={exit}>Exit</Nav.Link>
+                        </Nav>
+                    </>
+                    :
+                    <>
+                        <Nav>
+                        </Nav>
+                        <Nav className='ms-auto'>
+                            <Nav.Link href='#login' onClick={login}>Login</Nav.Link>
+                        </Nav>
+                    </>
+                        //       <>
+                        //       <Nav className="me-auto">
+                        //           <Nav.Link href='/home'>Home</Nav.Link>
+                        //           <Nav.Link href='/users'>Users</Nav.Link>
+                        //           <Nav.Link href='/about'>About</Nav.Link>
+                        //       </Nav>
+                        //       <Nav>
+                        //           <Nav.Link href='#exit' onClick={exit}>Exit</Nav.Link>
+                        //       </Nav>
+                        //   </>
+                        //   :
+                        //   <>
+                        //       <Nav className="me-auto">
+                        //       </Nav>
+                        //       <Nav >
+                        //           <Nav.Link href='#login' onClick={login}>Login</Nav.Link>
+                        //       </Nav>
+                        //   </>
+                    }
                 </Navbar.Collapse>
             </Container>
         </Navbar>
